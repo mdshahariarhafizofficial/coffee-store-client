29b45c8e4b734a0c0427f4883bc5e4a07309938e
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-05-17 12:33:18 +0600

    Get data from input field

--- src/Pages/SingUp/SingUp.jsx
+++ src/Pages/SingUp/SingUp.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
 const SingUp = () => {
+
+    // handle create user
+    const handleCreateUser = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const email = formData.get('email');
+        const password = formData.get('password');
+        console.log(email, password); 
+    }
+
     return (
         <div className='flex items-center justify-center my-10'>
-            <div className="w-full max-w-md p-4 rounded-md shadow sm:p-8 dark:bg-gray-50 dark:text-gray-800">
+            <div className="w-full max-w-md p-4 rounded-md shadow sm:p-8 bg-[#F4F3F0] dark:text-gray-800">
                 <h2 className="mb-3 text-3xl font-semibold text-center">Sing Up Now!</h2>
                 <p className="text-sm text-center dark:text-gray-600">Dont have account?
                     <a href="#" rel="noopener noreferrer" className="focus:underline hover:underline">Sign up here</a>
                 </p>
+
+                {/* GOOGLE */}
                 <div className="my-6 space-y-4">
                     <button aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-600 focus:dark:ring-violet-600">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
@@ -17,15 +30,20 @@ const SingUp = () => {
                     </button>
 
                 </div>
+
                 <div className="flex items-center w-full my-4">
                     <hr  className="w-full dark:text-gray-600" />
                     <p className="px-3 dark:text-gray-600">OR</p>
                     <hr  className="w-full dark:text-gray-600" />
                 </div>
-                <form noValidate="" action="" className="space-y-8">
+
+                {/* Form */}
+                <form onSubmit={handleCreateUser}
+                className="space-y-8">
                     <div className="space-y-4">
                         <div className="space-y-2">
                             <label htmlFor="email" className="block text-sm">Email address</label>
+                            {/* Email */}
                             <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600" />
                         </div>
                         <div className="space-y-2">
@@ -33,11 +51,13 @@ const SingUp = () => {
                                 <label htmlFor="password" className="text-sm">Password</label>
 
                             </div>
+                            {/* Password */}
                             <input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600" />
                         </div>
                     </div>
-                    <button type="button" className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50">Sign in</button>
+                    <button type="submit" className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50">Sign in</button>
                 </form>
+
             </div>
         </div>
     );
